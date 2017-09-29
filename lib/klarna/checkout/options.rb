d8873ef0aacc34cd09c75b77a2f5d3aefb5cfb95
Peter Backman <peter@iostream.cc>
2017-09-29 09:38:12 +0200

    Added color button options.

--- lib/klarna/checkout/options.rb
+++ lib/klarna/checkout/options.rb
@@ -5,11 +5,13 @@ module Klarna
     class Options < Resource
       include ActiveModel::Validations
 
-      attr_accessor :allowed_customer_types
+      attr_accessor :allowed_customer_types, :color_button, :color_button_text
 
       def as_json
         json_sanitize({
-          :allowed_customer_types               => @allowed_customer_types
+          :allowed_customer_types               => @allowed_customer_types,
+          :color_button                         => @color_button,
+          :color_button_text                    => @color_button_text
         })
       end
     end
